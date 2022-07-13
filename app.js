af858666d08126d3d483463f808aab2356f5bf35
Aditya Aggarwal <85567110+WhackyBoyg0d@users.noreply.github.com>
2022-07-14 03:18:28 +0530

    final

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
+var items  = [];
 const app = express();
 
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get("/",function(req, res){
     
     var today = new Date();
-    var currentDay = today.getDay();
-    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    var day = '';
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
 
 
 
-    if(today.getDay() == 6 || today.getDay == 0) {
-        day = days[today.getDay()];
-    }
-    else{
-       day = days[today.getDay()];
 
-    }
-
-
-    res.render("list", {kindOfDay: day});
+    res.render("list", {kindOfDay: day, newListItems: items});
 });
+
+app.post("/", function(req, res){
+    var item  = req.body.newItem;
+
+    items.push(item);
+    
+    res.redirect("/");
+    
+})
 
 app.listen(500, function(){
     console.log("Server is running at 500");
